9cf410696a6843c745894a06b68fbfb9611bc44b
Laís Domingos dos Santos <50579591+LaisDomingos@users.noreply.github.com>
2024-04-08 14:05:16 -0300

    Registrar

--- projeto-todo/todo/todo/urls.py
+++ projeto-todo/todo/todo/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
