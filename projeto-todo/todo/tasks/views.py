73d0f4e447f4428c826199ebfc3e9e8a537f147b
Laís Domingos dos Santos <50579591+LaisDomingos@users.noreply.github.com>
2024-04-15 05:43:25 -0300

    Views atualizada

--- projeto-todo/todo/tasks/views.py
+++ projeto-todo/todo/tasks/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .forms import TaskForm
 from django.contrib import messages
+import datetime
 
 from .models import Task
 
@@ -11,19 +12,26 @@ from .models import Task
 def taskList(request):
 
     search = request.GET.get('search')
+    filter = request.GET.get('filter')
+    tasksDoneRecently = Task.objects.filter(done='done', updated_at__gt=datetime.datetime.now()-datetime.timedelta(days=30)).count()
+    tasksDone = Task.objects.filter(done='done', user=request.user).count()
+    tasksDoing = Task.objects.filter(done='doing', user=request.user).count()
 
     if search:
         tasks = Task.objects.filter(title__icontains=search, user=request.user)
+    elif filter:
+        tasks = Task.objects.filter(done=filter, user=request.user)
     else:
         tasks_list = Task.objects.all().order_by('-created_at').filter(user=request.user)
 
 
-        paginator = Paginator(tasks_list, 3)
+        paginator = Paginator(tasks_list, 5)
 
         page = request.GET.get('page')
         tasks = paginator.get_page(page)
 
-    return render(request, 'tasks/list.html', {'tasks':tasks})
+    return render(request, 'tasks/list.html', 
+        {'tasks':tasks, 'tasksrecently': tasksDoneRecently, 'tasksdone': tasksDone, 'tasksdoing': tasksDoing })
 
 @login_required
 def taskView(request, id):
